baf8c7c42a6ca6326bdb9c93c8a42a4c60ef44cd
Stefhany Santos <stefhany.emilly@hotmail.com>
2025-05-28 22:42:46 -0300

    feat: validate unique values by column

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ df = pd.read_csv('database/insurance_brasil_simulado.csv')
 print(df.head())
 
 df.head()
+df.describe()
+# df.info()
+
+print(df.describe());
+# print(df.info());
+
+for column in ['sexo', 'fumante', 'regiao']:
+    print(df[column].value_counts())
+    print(f"Valores únicos da coluna {column}: {df[column].unique()}")
+
+
