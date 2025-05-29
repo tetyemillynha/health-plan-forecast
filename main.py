7bf6103c29574091c66a9e9fee2a2277dc5fb174
Stefhany Santos <stefhany.emilly@hotmail.com>
2025-05-28 22:56:54 -0300

    feat: validate null values

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pandas as pd
 
+# Carregue a base de dados e explore suas características;
 df = pd.read_csv('database/insurance_brasil_simulado.csv')
-
 print(df.head())
 
-# Analisando os dados
+
+# Analise estatísticas descritivas e visualize distribuições relevantes.
 df.head()
 df.describe()
-print(df.info());
-print(df.describe());
+# print(df.info());
+# print(df.describe());
 
 # Analisando os valores nulos de cada coluna (numericas)
 print(df.isnull().sum());
 
 # Analisando os valores únicos de cada coluna (categoricas)
 for column in ['sexo', 'fumante', 'regiao']:
-    print(df[column].value_counts())
+    # print(df[column].value_counts())
     print(f"Valores únicos da coluna {column}: {df[column].unique()}")
 
 # Analisando os valores nulos de cada coluna (numericas)
 df.isnull().sum()
 
+
+# Vizualização de distribuições relevantes.
+
+# Histograma por região
+print(df.groupby('regiao')['encargos'].mean())
