97efb145413ff36d97a267b0d18e434121e89d1e
Stefhany Santos <stefhany.emilly@hotmail.com>
2025-05-28 22:47:41 -0300

    feat: validate null values

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ df = pd.read_csv('database/insurance_brasil_simulado.csv')
 
 print(df.head())
 
+# Analisando os dados
 df.head()
 df.describe()
-# df.info()
-
+print(df.info());
 print(df.describe());
-# print(df.info());
 
+# Analisando os valores nulos de cada coluna (numericas)
+print(df.isnull().sum());
+
+# Analisando os valores únicos de cada coluna (categoricas)
 for column in ['sexo', 'fumante', 'regiao']:
     print(df[column].value_counts())
     print(f"Valores únicos da coluna {column}: {df[column].unique()}")
 
+# Analisando os valores nulos de cada coluna (numericas)
+df.isnull().sum()
 
